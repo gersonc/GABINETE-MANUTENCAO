25f85c75cd59bd72f921989c0985669334112142
gersonc <gersonc@wxy.com.br>
2018-03-16 21:34:39 -0300

    m

--- src/app/dados/dados.service.ts
+++ src/app/dados/dados.service.ts
@@ -1,11 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import {CadsitePaginacaoInterface} from './dados.interface';
+
 
 @Injectable()
 export class DadosService {
 
+  private gab$: Observable<CadsitePaginacaoInterface>;
+  private siteUrl = 'http://slimgn01/cadastro/incluirverificanome';
+
   constructor(private http: HttpClient) {
+  }
+
+  getSiteLer(): Observable<CadsitePaginacaoInterface> {
+    return this.http.get<CadsitePaginacaoInterface>(this.siteUrl);
   }
 
 }
