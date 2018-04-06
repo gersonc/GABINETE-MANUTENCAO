7ea964c91e991bcdfcdef29a567e82ae716814cf
Gerson Conde <gersonc@users.noreply.github.com>
2018-04-06 07:36:05 -0300

    m

--- src/app/dados/dados.service.ts
+++ src/app/dados/dados.service.ts
@@ -34,8 +34,11 @@ export class DadosService {
     if (order) {
       url += '&_order=' + order;
     }
+
+    // this.headers.set('Access-Control-Allow-Origin', '*');
+
     console.log('api->', url);
-    return this.http.get<CadsitePaginacaoInterface>(this.siteUrl);
+    return this.http.get<CadsitePaginacaoInterface>(url);
   }
 
 }
