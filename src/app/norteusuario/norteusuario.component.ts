d8b27dac94221c0ca2040ca998ce0dbd6fdd1df8
Gerson Conde <gersonc@users.noreply.github.com>
2018-04-08 18:47:59 -0300

    m

---
+++ src/app/norteusuario/norteusuario.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-norteusuario',
+  templateUrl: './norteusuario.component.html',
+  styleUrls: ['./norteusuario.component.css']
+})
+export class NorteusuarioComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/norteusuario/norteusuario.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {TableModule} from 'primeng/table';
+import {MultiSelectModule} from 'primeng/multiselect';
+
+import {DadosModule} from '../dados/dados.module';
+import {NorteusuarioeRoutingModule} from './norteusuario-routing.module';
+import {NorteusuarioComponent} from './norteusuario.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    TableModule,
+    MultiSelectModule,
+    DadosModule,
+    NorteusuarioeRoutingModule
+  ],
+  declarations: [NorteusuarioComponent]
+})
+export class NorteusuarioModule {
+}
